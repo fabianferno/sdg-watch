424e62f1b86275429391bdef05a634f910a9485d
Fabian Ferno <fabianferno@gmail.com>
2023-03-24 05:30:25 +0530

    Add dashboard

--- src/pages/CompanyDashboard.jsx
+++ src/pages/CompanyDashboard.jsx
@@ -1,46 +1,187 @@
-import Card from "react-bootstrap/Card";
-import { ProjectModal } from "../components/ProjectModal";
-
-function ProjectCard() {
-  return (
-    <div className="col-md-3 my-4">
-      <Card
-        className="border-primary"
-        bg="black"
-        text="light"
-        style={{ width: "18rem" }}
-      >
-        {/* <Card.Img
-          variant="top"
-          src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
-        /> */}
-        <Card.Body>
-          <h6 className="fw-bold text-white">Ethereum Name Service</h6>
-          <Card.Text className="fw-light">
-            Some quick example text to build on the card title and make up the
-            bulk of the content.
-          </Card.Text>
-
-          <ProjectModal />
-        </Card.Body>
-      </Card>
-    </div>
-  );
-}
+import { ProjectCard } from "../components/ProjectsCard";
+import { Card, Col, Row } from "react-bootstrap";
 
 export default function CompanyDashboard() {
   return (
-    <div>
-      <h1>Company Dashboard Projects</h1>
+    <div className="mt-3">
+      <h1 className="text-white fw-bold ">Project Name</h1>
       <div>
-        <h4 className="text-white fw-bold">View projects to fund</h4>
+        <h5 className="">View projects to fund</h5>
         <hr />
         <div className="row">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          {/* Section 1 */}
+          <div className="col-md-4 my-4">
+            <Card
+              style={{
+                height: "12rem",
+                background:
+                  "linear-gradient(-120deg, rgba(0,0,0,1) 0%, rgba(36,36,36,1) 100%)",
+              }}
+              className="border-dark"
+              bg="black"
+              text="light"
+            >
+              <Card.Body>
+                <h6 className="fw-bold text-white">Ethereum Name Service</h6>
+                <Card.Text className="fw-light">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+
+          {/* Section 2 */}
+          <div className="col-md-4 my-4">
+            <Card
+              style={{
+                height: "12rem",
+                background:
+                  "linear-gradient(-120deg, rgba(0,0,0,1) 0%, rgba(36,36,36,1) 100%)",
+              }}
+              className="border-dark"
+              bg="black"
+              text="light"
+            >
+              <Card.Body>
+                <h6 className="fw-bold text-white">Ethereum Name Service</h6>
+                <Card.Text className="fw-light">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+
+          {/* Section 3 */}
+          <div className="col-md-4 my-4">
+            <Card
+              className="border-dark"
+              style={{
+                height: "12rem",
+                background:
+                  "linear-gradient(-120deg, rgba(0,0,0,1) 0%, rgba(36,36,36,1) 100%)",
+              }}
+              text="light"
+            >
+              <Card.Body>
+                <h6 className="fw-bold text-white">
+                  Companies that have claimed
+                </h6>
+                <Card.Text className="fw-light mt-3">
+                  {/* Generate a list of companies that have claimed with bootstrap 5 classes */}
+                  - Company 1
+                  <br />
+                  - Company 2
+                  <br />- Company 3
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        {/* Section 4 */}
+        <div className="row m-1">
+          <Card
+            className="border-dark"
+            style={{
+              background:
+                "linear-gradient(-120deg, rgba(0,0,0,1) 0%, rgba(36,36,36,1) 100%)",
+            }}
+            text="light"
+          >
+            <Card.Body>
+              {/* Section 4.1 */}
+              <Row className="mt-2">
+                <Col xs={1}>
+                  <h6 className="  text-white">Direct Positive</h6>
+                </Col>
+                {Array.from({ length: 17 }).map((_, index) => (
+                  <Col key={index}>
+                    <div
+                      className="rounded border border-primary "
+                      style={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                      }}
+                    ></div>
+                  </Col>
+                ))}
+              </Row>
+
+              {/* Section 4.2 */}
+              <Row className="mt-2">
+                <Col xs={1}>
+                  <h6 className=" text-white">Indirect Positive</h6>
+                </Col>
+                {Array.from({ length: 17 }).map((_, index) => (
+                  <Col key={index}>
+                    <div
+                      className="rounded border border-primary "
+                      style={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                      }}
+                    ></div>
+                  </Col>
+                ))}
+              </Row>
+
+              {/* Section 4.3 */}
+              <Row className="mt-2">
+                <Col xs={1}>
+                  <h6 className="text-white">No Impact</h6>
+                </Col>
+                {Array.from({ length: 17 }).map((_, index) => (
+                  <Col key={index}>
+                    <div
+                      className="rounded border border-info "
+                      style={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                      }}
+                    ></div>
+                  </Col>
+                ))}
+              </Row>
+
+              {/* Section 4.4 */}
+              <Row className="mt-2">
+                <Col xs={1}>
+                  <h6 className="  text-white">Indirect Negative</h6>
+                </Col>
+                {Array.from({ length: 17 }).map((_, index) => (
+                  <Col key={index}>
+                    <div
+                      className="rounded border border-danger "
+                      style={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                      }}
+                    ></div>
+                  </Col>
+                ))}
+              </Row>
+
+              {/* Section 4.45 */}
+              <Row className="mt-2">
+                <Col xs={1}>
+                  <h6 className=" text-white">Direct Negative</h6>
+                </Col>
+                {Array.from({ length: 17 }).map((_, index) => (
+                  <Col key={index}>
+                    <div
+                      className="rounded border border-danger "
+                      style={{
+                        width: "2.5rem",
+                        height: "2.5rem",
+                      }}
+                    ></div>
+                  </Col>
+                ))}
+              </Row>
+            </Card.Body>
+          </Card>
         </div>
       </div>
     </div>
