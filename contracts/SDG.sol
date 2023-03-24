a876928fbcf3b4c835fab69edbf804e17c6a38d4
surbhit14 <surbhitagrawal716@gmail.com>
2023-03-24 09:02:48 +0530

    deployment

--- contracts/SDG.sol
+++ contracts/SDG.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC1155, Ownable {
+contract SDG is ERC1155, Ownable {
 
     mapping(address => mapping(uint256 => uint256)) private _balances;
     mapping(address => bool) private _whitelist;
